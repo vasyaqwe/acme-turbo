dfba3c61508cb3cd797064cc83ce484b4c23e2fc
vasyaqwe <elekton.fafik@gmail.com>
2024-05-28 15:02:24 +0300

    fixes

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,9 +1,4 @@
-import { useState } from "react"
-import { Pressable, Text, TextInput, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
-import { Stack } from "expo-router"
-import { api } from "@/utils/api"
-import { FlashList } from "@shopify/flash-list"
 
 // function PostCard(props: {
 //    post: RouterOutputs["post"]["all"][number]
@@ -36,63 +31,63 @@ import { FlashList } from "@shopify/flash-list"
 //    )
 // }
 
-function CreatePost() {
-   // const utils = api.useUtils()
+// function CreatePost() {
+//    // const utils = api.useUtils()
 
-   // const [title, setTitle] = useState("")
-   // const [content, setContent] = useState("")
+//    // const [title, setTitle] = useState("")
+//    // const [content, setContent] = useState("")
 
-   // const { mutate, error } = api.post.create.useMutation({
-   //    async onSuccess() {
-   //       setTitle("")
-   //       setContent("")
-   //       await utils.post.all.invalidate()
-   //    },
-   // })
+//    // const { mutate, error } = api.post.create.useMutation({
+//    //    async onSuccess() {
+//    //       setTitle("")
+//    //       setContent("")
+//    //       await utils.post.all.invalidate()
+//    //    },
+//    // })
 
-   return (
-      <View className="mt-4 flex gap-2">
-         {/* <TextInput
-            className=" items-center rounded-md border border-input bg-background px-3 text-lg leading-[1.25] text-foreground"
-            value={title}
-            onChangeText={setTitle}
-            placeholder="Title"
-         />
-         {error?.data?.zodError?.fieldErrors.title && (
-            <Text className="mb-2 text-destructive">
-               {error.data.zodError.fieldErrors.title}
-            </Text>
-         )}
-         <TextInput
-            className="items-center rounded-md border border-input bg-background px-3  text-lg leading-[1.25] text-foreground"
-            value={content}
-            onChangeText={setContent}
-            placeholder="Content"
-         />
-         {error?.data?.zodError?.fieldErrors.content && (
-            <Text className="mb-2 text-destructive">
-               {error.data.zodError.fieldErrors.content}
-            </Text>
-         )}
-         <Pressable
-            className="flex items-center rounded bg-primary p-2"
-            onPress={() => {
-               mutate({
-                  title,
-                  content,
-               })
-            }}
-         >
-            <Text className="text-foreground">Create</Text>
-         </Pressable>
-         {error?.data?.code === "UNAUTHORIZED" && (
-            <Text className="mt-2 text-destructive">
-               You need to be logged in to create a post
-            </Text>
-         )} */}
-      </View>
-   )
-}
+//    return (
+//       <View className="mt-4 flex gap-2">
+//          {/* <TextInput
+//             className=" items-center rounded-md border border-input bg-background px-3 text-lg leading-[1.25] text-foreground"
+//             value={title}
+//             onChangeText={setTitle}
+//             placeholder="Title"
+//          />
+//          {error?.data?.zodError?.fieldErrors.title && (
+//             <Text className="mb-2 text-destructive">
+//                {error.data.zodError.fieldErrors.title}
+//             </Text>
+//          )}
+//          <TextInput
+//             className="items-center rounded-md border border-input bg-background px-3  text-lg leading-[1.25] text-foreground"
+//             value={content}
+//             onChangeText={setContent}
+//             placeholder="Content"
+//          />
+//          {error?.data?.zodError?.fieldErrors.content && (
+//             <Text className="mb-2 text-destructive">
+//                {error.data.zodError.fieldErrors.content}
+//             </Text>
+//          )}
+//          <Pressable
+//             className="flex items-center rounded bg-primary p-2"
+//             onPress={() => {
+//                mutate({
+//                   title,
+//                   content,
+//                })
+//             }}
+//          >
+//             <Text className="text-foreground">Create</Text>
+//          </Pressable>
+//          {error?.data?.code === "UNAUTHORIZED" && (
+//             <Text className="mt-2 text-destructive">
+//                You need to be logged in to create a post
+//             </Text>
+//          )} */}
+//       </View>
+//    )
+// }
 
 export default function Index() {
    // const utils = api.useUtils()
--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -2,14 +2,10 @@ import { fileURLToPath } from "url"
 
 /** @typedef {import("prettier").Config} PrettierConfig */
 /** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
-/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
 
-/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+/** @type { PrettierConfig | TailwindConfig } */
 const config = {
-   plugins: [
-      "@ianvs/prettier-plugin-sort-imports",
-      "prettier-plugin-tailwindcss",
-   ],
+   plugins: ["prettier-plugin-tailwindcss"],
    trailingComma: "es5",
    tabWidth: 3,
    semi: false,
@@ -18,23 +14,6 @@ const config = {
       new URL("../../tooling/tailwind/web.ts", import.meta.url)
    ),
    tailwindFunctions: ["cn", "cva"],
-   importOrder: [
-      "<TYPES>",
-      "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
-      "^(next/(.*)$)|^(next$)",
-      "^(expo(.*)$)|^(expo$)",
-      "<THIRD_PARTY_MODULES>",
-      "",
-      "<TYPES>^@acme",
-      "^@acme/(.*)$",
-      "",
-      "<TYPES>^[.|..|~]",
-      "^~/",
-      "^[../]",
-      "^[./]",
-   ],
-   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-   importOrderTypeScriptVersion: "4.4.0",
 }
 
 export default config
