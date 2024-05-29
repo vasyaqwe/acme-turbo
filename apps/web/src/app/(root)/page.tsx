88fed07afd6151f2f2dfbd495bd4c50876dc486b
vasyaqwe <elekton.fafik@gmail.com>
2024-05-29 12:58:47 +0300

    vercel prod error 😑😑😑

--- apps/web/src/app/(root)/page.tsx
+++ apps/web/src/app/(root)/page.tsx
@@ -1,10 +1,27 @@
+import { Expenses } from "@/app/(root)/_components/expenses"
 import { Intro } from "@/app/(root)/_components/intro"
+import { api } from "@/trpc/server"
+import { Suspense } from "react"
+import {
+   Table,
+   TableBody,
+   TableCell,
+   TableHead,
+   TableHeader,
+   TableRow,
+} from "@acme/ui/table"
+import { Card } from "@acme/ui/card"
+import { Skeleton } from "@acme/ui/skeleton"
+import { ErrorBoundary } from "react-error-boundary"
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline"
 
 export default function Page() {
+   void api.expense.getAll()
+
    return (
       <div className="w-full py-6">
          <Intro />
-         {/* 
+
          <Card className="mt-5 p-5 pt-4">
             <h2 className="mb-4 text-xl font-bold">Expenses</h2>
             <ErrorBoundary
@@ -50,7 +67,7 @@ export default function Page() {
                   </TableBody>
                </Table>
             </ErrorBoundary>
-         </Card> */}
+         </Card>
       </div>
    )
 }
