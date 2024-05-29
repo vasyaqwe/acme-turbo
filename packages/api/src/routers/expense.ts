0bc1167c2708f16c7b0dc3078052ad0d65dd2755
vasyaqwe <elekton.fafik@gmail.com>
2024-05-29 13:52:29 +0300

    vercel prod error 😑😑😑

--- packages/api/src/routers/expense.ts
+++ packages/api/src/routers/expense.ts
@@ -7,15 +7,19 @@ import { TRPCError } from "@trpc/server"
 
 export const expense = createTRPCRouter({
    getAll: publicProcedure.query(async ({ ctx }) => {
-      const user = ctx.session?.user
-      if (!user?.id) return []
+      try {
+         const user = ctx.session?.user
+         if (!user?.id) return []
 
-      // const res = await ctx.db
-      //    .select()
-      //    .from(expenses)
-      //    .where(eq(expenses.userId, user.id))
+         // const res = await ctx.db
+         //    .select()
+         //    .from(expenses)
+         //    .where(eq(expenses.userId, user.id))
 
-      throw new TRPCError({ code: "NOT_IMPLEMENTED" })
+         throw new TRPCError({ code: "NOT_IMPLEMENTED" })
+      } catch (error) {
+         console.log(error)
+      }
    }),
    getAll2: publicProcedure.query(async ({ ctx }) => {
       const user = ctx.session?.user
