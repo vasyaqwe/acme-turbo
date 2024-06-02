587faee476bbe7989c051db7a0822a62abbe0d46
vasyaqwe <elekton.fafik@gmail.com>
2024-06-02 17:42:12 +0300

    test prefetch

--- apps/web/src/components/link.tsx
+++ apps/web/src/components/link.tsx
@@ -1,20 +1,19 @@
 import NextLink from "next/link"
-import { useRouter } from "next/navigation"
 import { type ComponentPropsWithRef } from "react"
 
 export const Link = (props: ComponentPropsWithRef<typeof NextLink>) => {
-   const router = useRouter()
    return (
       <NextLink
          {...props}
-         onMouseEnter={(e) => {
-            const href =
-               typeof props.href === "string" ? props.href : props.href.href
-            if (href) {
-               router.prefetch(href)
-            }
-            return props.onMouseEnter?.(e)
-         }}
+         // onMouseEnter={(e) => {
+         //    const href =
+         //       typeof props.href === "string" ? props.href : props.href.href
+         //    if (href) {
+         //       router.prefetch(href)
+         //    }
+         //    return props.onMouseEnter?.(e)
+         // }}
+         prefetch
       />
    )
 }
