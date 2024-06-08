67aac648c8e0f3593938105c28a35726920de6e4
vasyaqwe <elekton.fafik@gmail.com>
2024-06-08 14:48:30 +0300

    remove ps1 files

--- deploy.ps1
+++
@@ -1,3 +0,0 @@
-fly deploy `
-    --config fly.web.toml `
-    --build-secret DATABASE_URL=postgres://default:khGt2c9iNIex@ep-wispy-thunder-a21y1x5q-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require
